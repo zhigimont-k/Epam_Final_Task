4668ec6b39f66b48abb1768f7ac99b8db5e83f40
Karina <zhigimont.k@gmail.com>
2018-07-13 15:31:15 +0300

    Add filter

---
+++ src/main/java/by/epam/web/filter/EncodingFilter.java
@@ -0,0 +1,26 @@
+package by.epam.web.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class EncodingFilter implements Filter {
+    private String code;
+
+    public void init(FilterConfig fConfig) throws ServletException {
+        code = fConfig.getInitParameter("encoding");
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        String codeRequest = request.getCharacterEncoding();
+        if (code != null && !code.equalsIgnoreCase(codeRequest)) {
+            request.setCharacterEncoding(code);
+            response.setCharacterEncoding(code);
+        }
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+        code = null;
+    }
+}
